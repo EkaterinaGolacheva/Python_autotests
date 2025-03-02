6c6dea38181d84a86f2b0bd044f7056007c2a3ce
EkaterinaG. <107884267+EkaterinaGolacheva@users.noreply.github.com>
2025-03-02 20:18:30 +0300

    Add files via upload

---
+++ QA_Python/main.py
@@ -0,0 +1,63 @@
+import requests
+
+URL = 'https://api.pokemonbattle.ru/v2'
+TOKEN = '9bd4ff17692c62f5dd2f523875fb9295'
+HEADER = {'Content-Type': 'application/json', 'trainer_token' : TOKEN}
+
+BODY_REG = {
+    "trainer_token": TOKEN,
+    "email": "ekaterina2034@gmail.com",
+    "password": "Iloveqa123"
+}
+
+BODY_CONFIRMATION = {
+    "trainer_token": TOKEN  
+}
+
+BODY_CREATE = {"name": "generate",
+    "photo_id": -1}
+
+BODY_CATCHE = {
+    "pokemon_id": "247386"
+}
+
+BODY_BATTLE = {"attacking_pokemon": "247386",
+    "defending_pokemon": "248420"}
+
+BODY_NAME = {
+    "pokemon_id": "248431",
+    "name": "Lakky",
+    "photo_id": 45
+}
+
+BODY_KNOCKOUT = {
+    "pokemon_id": "247385"
+}
+
+
+
+response_create = requests.post(url = f'{URL}/pokemons', headers = HEADER, json = BODY_CREATE)
+print(response_create.text)
+
+response_name = requests.put(url = f'{URL}/pokemons', headers = HEADER, json = BODY_NAME)
+print(response_name.text)
+
+response_catche = requests.post(url = f'{URL}/trainers/add_pokeball', headers = HEADER, json = BODY_CATCHE)
+print(response_catche.text)
+
+
+
+
+'''response_knockout = requests.post(url = f'{URL}/pokemons/knockout', headers = HEADER, json = BODY_KNOCKOUT)
+print(response_knockout.text)
+
+response_battle = requests.post(url = f'{URL}/battle', headers = HEADER, json = BODY_BATTLE)
+print(response_battle.text)
+MESSAGE = response_battle.json()['message']
+print(MESSAGE)
+
+response = requests.post(url = f'{URL}/trainers/reg', headers = HEADER, json = BODY_REG)
+print(response.text)
+
+response_confirmation = requests.post(url = f'{URL}/trainers/confirm_email', headers = HEADER, json = BODY_CONFIRMATION)
+print(response_confirmation.text)'''
---
+++ QA_Python/tests/test_pokemons.py
@@ -0,0 +1,29 @@
+import requests
+import pytest
+
+URL = 'https://api.pokemonbattle.ru/v2'
+TOKEN = '9bd4ff17692c62f5dd2f523875fb9295'
+HEADER = {'Content-Type': 'application/json', 'trainer_token' : TOKEN}
+TRAINER_ID = '32911'
+
+def test_status_code():
+    response = requests.get(url = f'{URL}/trainers', params = {'trainer_id' : TRAINER_ID})
+    assert response.status_code == 200
+
+def test_name_of_trainer():
+    response_name = requests.get(url = f'{URL}/trainers', params = {'trainer_id' : TRAINER_ID})
+    assert response_name.json()["data"][0]["trainer_name"] == 'Koko'
+
+def test_city():
+    response_city = requests.get(url = f'{URL}/trainers', params = {'trainer_id' : TRAINER_ID})
+    assert response_city.json()["data"][0]["city"] == 'Kokoville'
+
+'''def test_premium():
+    response_premium = requests.get(url = f'{URL}/trainers', params = {'trainer_id' : TRAINER_ID})
+    assert response_premium.json()["data"][0]["is_premium"] == 'false' ПОЧЕМУ ЭТОТ ТЕСТ НЕ РАБОТАЕТ?'''
+
+
+@pytest.mark.parametrize('key, value', [('trainer_name', 'Koko'), ('id', TRAINER_ID), ('city', 'Kokoville')])
+def test_parametrize(key, value): 
+    response_parametrize = requests.get(url = f'{URL}/trainers', params = {'trainer_id' : TRAINER_ID})
+    assert response_parametrize.json()["data"][0][key] == value
