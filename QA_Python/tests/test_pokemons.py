8a132e98caf19262576a9127a073901e3cc1ab18
EkaterinaG. <107884267+EkaterinaGolacheva@users.noreply.github.com>
2025-03-03 15:03:47 +0300

    Update test_pokemons.py
    Changed token

--- QA_Python/tests/test_pokemons.py
+++ QA_Python/tests/test_pokemons.py
@@ -2,7 +2,7 @@ import requests
 import pytest
 
 URL = 'https://api.pokemonbattle.ru/v2'
-TOKEN = '9bd4ff17692c62f5dd2f523875fb9295'
+TOKEN = '11111111111111111111111111'
 HEADER = {'Content-Type': 'application/json', 'trainer_token' : TOKEN}
 TRAINER_ID = '32911'
 
